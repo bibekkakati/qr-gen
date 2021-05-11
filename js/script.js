c0f020650c5280dda581b2ecd3bd68804aa39e2f
bibekkakati <bibekkakati0@gmail.com>
2021-05-12 00:28:18 +0530

    white padding added to QRCode

--- js/script.js
+++ js/script.js
@@ -12,8 +12,8 @@ function main() {
 	const qrdivClassName = "qrdiv";
 
 	var QR_CODE = new QRCode("qrcode", {
-		width: 220,
-		height: 220,
+		width: 260,
+		height: 260,
 		colorDark: "#000000",
 		colorLight: "#ffffff",
 		correctLevel: QRCode.CorrectLevel.H,
@@ -32,13 +32,38 @@ function main() {
 		const classList = dataBox.classList;
 
 		if (classList.contains(errorClassName)) {
+			// Removing error class
 			dataBox.className = dataBoxClassName;
 		}
 	};
 
 	downloadBtn.onclick = function (e) {
-		const image = qrcode.getElementsByTagName("img")[0].currentSrc;
-		const filename = "QR_Code_" + Date.now() + getExtension(image);
+		// Image tag
+		const img = qrcode.getElementsByTagName("img")[0];
+		// Canvas tag
+		const canvas = qrcode.getElementsByTagName("canvas")[0];
+
+		// Padding to QRCode
+		const padding = 40;
+
+		// Adding padding to width and height
+		canvas.width = canvas.width + padding;
+		canvas.height = canvas.height + padding;
+
+		// Canvas context
+		const context = canvas.getContext("2d");
+		// Clearing previous content
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		// Making the background white
+		context.fillStyle = "#ffffff";
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		// Adding the image of QRCode
+		// x and y are padding / 2
+		context.drawImage(img, padding / 2, padding / 2);
+
+		// Getting base64 url
+		const image = canvas.toDataURL("image/png", 1);
+		const filename = "QR_Code_" + Date.now() + ".png";
 		downloadImage(image, filename);
 	};
 
@@ -48,6 +73,7 @@ function main() {
 			prevClassName + " " + errorClassName + " " + shakeClassName;
 		vibrate();
 		setTimeout(() => {
+			// Reset class
 			dataBox.className = prevClassName + " " + errorClassName;
 		}, 500);
 	}
@@ -55,6 +81,7 @@ function main() {
 	function generateQRCode(data) {
 		QR_CODE.clear();
 		QR_CODE.makeCode(data);
+		// Show QRCode div
 		qrdiv.className = qrdivClassName;
 	}
 
@@ -65,6 +92,7 @@ function main() {
 	}
 
 	function downloadImage(image, filename) {
+		// Creating hidden <a> tag to download
 		var element = document.createElement("a");
 		element.setAttribute("href", image);
 		element.setAttribute("download", filename);
@@ -72,24 +100,6 @@ function main() {
 		document.body.appendChild(element);
 		element.click();
 		document.body.removeChild(element);
-	}
-
-	function getExtension(base64) {
-		let extension = ".";
-		if (!base64) {
-			throw new Error("Base64 url is required!");
-		}
-		let start = false;
-		for (let i = 0; i < base64.length; i++) {
-			if (base64[i] == "/") {
-				start = true;
-			} else if (base64[i] == ";") {
-				break;
-			} else if (start) {
-				extension += base64[i];
-			}
-		}
-		return extension;
 	}
 }
 
